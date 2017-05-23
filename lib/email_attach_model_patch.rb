c71b29526483306bf4f0f7cbea21d7a74c3814ec
Peter Pfläging <peter@pflaeging.net>
2017-05-23 15:01:30 +0200

    - Changed Filename to timestamp! - made the raw attachment to correct filetype (.EML) and mime_type (message/rfc822)

--- lib/email_attach_model_patch.rb
+++ lib/email_attach_model_patch.rb
@@ -1,3 +1,4 @@
+# patched by Peter Pfläging <peter@pflaeging.net>
 require_dependency 'mail_handler'
 
 module EmailAttachMailHandlerPatch
@@ -8,12 +9,15 @@ module EmailAttachMailHandlerPatch
       alias_method_chain :add_attachments, :raw_attachment
     end
   end
-  
+
   module InstanceMethods
     # Adds a rates tab to the user administration page
     def add_attachments_with_raw_attachment(obj)
       add_attachments_without_raw_attachment(obj)
       # add raw email as an attachment
+      # Generate Timestamp for filename (Peter Pfläging)
+      receivetime = Time.new
+      attachname = 'eMail-' + receivetime.strftime("%Y%m%d-%H%M%S%z") + '.'
       logger.info "MailHandler: adding raw attachments"
 
       if (parts = email.all_parts.select {|p| p.mime_type == 'text/html'}).present?
@@ -23,20 +27,23 @@ module EmailAttachMailHandlerPatch
             Redmine::CodesetUtil.to_utf8(p.body.decoded, body_charset)
           end.join("</hr>")
 
+          # Generate a file with the main content as HTML file
           obj.attachments << Attachment.create(:container => obj,
             :file => html_parts,
-            :filename => 'Incoming Email.html',
+            :filename => attachname + 'html',
             :author => user,
             :content_type => 'text/html')
       end
 
+      # Generate a file from the e-mail Source as .EML file with content type message/RFC822
+      # Outlook and Apple Mail are opening these files direct! (PP)
       obj.attachments << Attachment.create(:container => obj,
         :file => email.raw_source,
-        :filename => 'Incoming Email Source.txt',
+        :filename => attachname + 'eml',
         :author => user,
-        :content_type => 'text/plain')
+        :content_type => 'message/rfc822')
     end
-    
+
   end
 end
 
